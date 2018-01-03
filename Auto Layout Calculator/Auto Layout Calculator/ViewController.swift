90e1fab261de73b73f3ff1aafcadbff99ec4ffb6
Shivam dev <shivamdev@developer.com>
2018-01-03 11:58:14 +0530

    Auto layout calculator with no functionality

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Shivam Dev on 03/01/18.
+//  Copyright © 2018 Shivam Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
